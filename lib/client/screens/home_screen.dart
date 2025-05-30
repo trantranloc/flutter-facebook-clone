d6fd78896533a60d54f294c7635c90cb82986bff
Đặng Duy Báu <duybau2004.com>
2025-05-30 07:17:58 +0700

    Cập nhật code nhánh feat/dangduybau

--- lib/client/screens/home_screen.dart
+++ lib/client/screens/home_screen.dart
@@ -164,7 +164,6 @@ class _HomeScreenState extends State<HomeScreen> {
       }
 
       allPosts.sort((a, b) => b.createdAt.compareTo(a.createdAt));
-
       setState(() {
         posts = allPosts;
       });
